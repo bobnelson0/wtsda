79443d99478865035d8243d7ac812d60098e1666
Robert Nelson <bob.nelson@gmail.com>
2013-06-11 09:28:44 -0700

    Start of Events pages

--- src/Wtsda/AppBundle/Controller/DefaultController.php
+++ src/Wtsda/AppBundle/Controller/DefaultController.php
@@ -9,11 +9,11 @@ class DefaultController extends CoreController
 {
     public function indexAction()
     {
-        $permissions = $this->getRepo('WtsdaCoreBundle:Permission')
-            ->findAllOrderedByResource();
-
-        $output = '<pre>' . print_r($permissions, true) . '</pre>';
-        //return new Response('Permissions:<br />' . $output);
+//        $permissions = $this->getRepo('WtsdaCoreBundle:Permission')
+//            ->findAllOrderedByResource();
+//
+//        $output = '<pre>' . print_r($permissions, true) . '</pre>';
+//        //return new Response('Permissions:<br />' . $output);
         return $this->_render('WtsdaAppBundle:Default:index');
     }
 }
