d9aa763775d0dba437de53230cfaccc75006902c
bobnelson0 <bob.nelson@gmail.com>
2014-04-25 21:31:43 -0700

    Initial framework for JS files

--- web/bundles/wtsdaadmin/js/admin.js
+++ web/bundles/wtsdaadmin/js/admin.js
@@ -1,11 +1,33 @@
 var Admin = Admin || function() {
     'use strict'
-    var consts = {},
+    var consts = {
+            rowCount: 50,
+            url: '/admin/'
+        },
+        listeners = {
+            links : function() {
+                $('a[href="#"]').on('click', function(event) {
+                    event.preventDefault();
+                });
+            }
+        }.
         priv = {
         },
         pub = {
             init : function() {
-
+                this.initListeners(listeners);
+            },
+            initListeners: function(listeners) {
+                // Add listeners
+                $.each(listeners, function (index, func) {
+                    func();
+                });
+            },
+            getRowCount : function () {
+              return consts.rowCount;
+            },
+            getAdminUrl : function() {
+                return consts.url;
             }
         };
     return pub;
